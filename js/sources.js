ecab040090826f5dba94c1f37b8ea44a33142f89
dunnillion-pixel <dunnillion@gmail.com>
2026-06-02 17:25:09 +0200

    Create sources.js

---
+++ js/sources.js
@@ -0,0 +1,21 @@
+const SourceService = {
+
+  create(type, title, rawText) {
+    return {
+      id: crypto.randomUUID(),
+      type,
+      title,
+      rawText,
+      createdAt: Date.now()
+    };
+  },
+
+  createTeamsSimulated(title, transcript) {
+    return this.create(
+      "teams-simulated",
+      title,
+      transcript
+    );
+  }
+
+};
